2a4ae1ac31ae7a24f55f0c8203811b7f2476ba18
guilla.lab@gmail.com <guilla.lab@gmail.com>
2022-03-14 17:52:35 +0000

    Test files

--- test.java
+++ test.java
@@ -1,1 +1,14 @@
 //test
+
+public class test {
+
+    private String message = "DEFAULT",
+
+    public void Test(){
+
+    }
+
+    public String DisplayDefault() {
+       return message
+    }
+}
